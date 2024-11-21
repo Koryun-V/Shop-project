175b649914e93c2f5077dab65aa9a669fced79d6
Narek <narekdarbinyan111@gmail.com>
2024-11-21 18:25:37 +0400

    i created a utills file with its Api.js and now we will do all requests in this file

---
+++ src/utills/Api.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+
+const api = axios.create({
+baseURL: "https://world-of-construction.onrender.com"
+})
+
+
+export default class Api{
+
+}

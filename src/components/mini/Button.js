b673eb4bce06a7d6eb6e2e2a4b8d4984846e0fcb
Koryun-V <koryun.vardanyan.2002@gmail.com>
2024-12-05 19:27:06 +0400

    home-6

--- src/components/mini/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({onClick,type,className,text}) => {
-    return (
-            <button className={className} onClick={onClick} type={type}>{text}</button>
-    );
-};
-
-export default Button;
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,8 @@
 import {combineReducers} from "redux";
-import {login} from "./login";
-import {registration} from "./registration";
+
 import {home} from "./home";
 
 export const rootReducer = combineReducers({
-    registration,
-    login,
+
     home
 })

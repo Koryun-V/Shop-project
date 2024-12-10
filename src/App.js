fe006931bfe3ed115c58af49f6d94efdb0cc2b02
Koryun-V <koryun.vardanyan.2002@gmail.com>
2024-12-10 18:48:57 +0400

    user-new

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./components/common/Layout";
 import Register from "./components/common/Register";
 import Home from "./components/common/Home";
-import Products from "./components/common/Products";
-import Category from "./components/common/Category";
-import Specialist from "./components/common/Specialist";
-import Contact from "./components/common/Contact";
 import Users from "./components/pages/Users";
 import Admin from "./components/pages/Admin";
 
@@ -15,10 +11,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>}/>
-        <Route path="/products" element={<Products/>}/>
-        <Route path="/category" element={<Category/>}/>
-        <Route path="/specialist" element={<Specialist/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
         <Route path="/register" element={<Register/>}/>
 
 
--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -11,123 +11,137 @@ import {getUserProfileRequest, updateUserProfileRequest, setProfile} from "../..
 import Error from "./Error";
 
 const fields = [
-  {name: 'firstName', label: 'First Name', type: 'text'},
-  {name: 'lastName', label: 'Last Name', type: 'text'},
+    {name: 'firstName', label: 'First Name', type: 'text'},
+    {name: 'lastName', label: 'Last Name', type: 'text'},
 ];
 
 const genderOptions = [
-  {value: 'Male', label: 'Male'},
-  {value: 'Female', label: 'Female'},
+    {value: 'Male', label: 'Male'},
+    {value: 'Female', label: 'Female'},
 ];
 
 const Users = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const profile = useSelector((state) => state.users.profile);
-  const user = useSelector((state) => state.users.user);
-  const error = useSelector((state) => state.users.error);
-
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  // Fetch user profile data
-  useEffect(() => {
-    (async () => {
-
-      setLoading(true);
-
-      await dispatch(getUserProfileRequest());
-
-      setLoading(false);
-    })();
-  }, []);
+    const profile = useSelector((state) => state.users.profile);
+    const user = useSelector((state) => state.users.user);
+    const error = useSelector((state) => state.users.error);
 
 
-  const onChange = (e, type) => {
-    const {name, value} = e.target;
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const updatedValue = type === 'date' ? moment(value).format('YYYY-MM-DD') : value;
+    // Fetch user profile data
+    useEffect(() => {
+        (async () => {
 
-    dispatch(setProfile({...profile, [name]: updatedValue}));
-  };
+            setLoading(true);
 
+            await dispatch(getUserProfileRequest());
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    await dispatch(updateUserProfileRequest(profile));
-
-    setIsSubmitting(false);
-  };
+            setLoading(false);
+        })();
+    }, []);
 
 
-  return (
-    <div className="user-profile-update">
-      {error ? <Error/>
+    const onChange = (e, type) => {
+        const {name, value} = e.target;
 
-        :loading ? (
-        <Loader/>
-      ) : (
-        <>
-          <h1>Personal Data</h1>
-          <form onSubmit={handleSubmit}>
-            {fields.map((field) => (
-              <div key={field.name} className="user-profile-block">
-                <Input
-                  label={field.label}
-                  name={field.name}
-                  value={profile[field.name]}
-                  onChange={onChange}
-                  type={field.type}
-                />
-              </div>
-            ))}
+        const updatedValue = type === 'date' ? moment(value).format('YYYY-MM-DD') : value;
 
-            <div>
-              <label>Date of Birth:</label>
-              <DatePiker
-                selected={profile.dateOfBirth ? new Date(profile.dateOfBirth) : null}
-                onChange={(date) => onChange({target: {name: 'dateOfBirth', value: date}}, 'date')}
-                showYearDropdown
-                showMonthDropdown
-                minDate={new Date("09-10-1950")}
-              />
+        dispatch(setProfile({...profile, [name]: updatedValue}));
+    };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+
+        await dispatch(updateUserProfileRequest(profile));
+
+        setIsSubmitting(false);
+    };
+
+
+    return (
+        <div className="section">
+            <div className="container">
+                <div className="container-user">
+                    {error ? <Error/>
+
+                        : loading ? (
+                            <Loader/>
+                        ) : (
+                            <>
+                                <h1>Personal Data</h1>
+                                <form onSubmit={handleSubmit}>
+                                    {fields.map((field) => (
+                                        <div className="field-block" key={field.id}>
+                                            <div key={field.name} style={{
+                                                height: 50,
+                                            }}>
+                                                <Input
+                                                    label={field.label}
+                                                    name={field.name}
+                                                    value={profile[field.name]}
+                                                    onChange={onChange}
+                                                    type={field.type}
+                                                    className="input"
+                                                />
+                                            </div>
+                                        </div>
+                                    ))}
+
+                                    <div>
+                                        <label>Date of Birth:</label>
+                                        <DatePiker
+                                            selected={profile.dateOfBirth ? new Date(profile.dateOfBirth) : null}
+                                            onChange={(date) => onChange({
+                                                target: {
+                                                    name: 'dateOfBirth',
+                                                    value: date
+                                                }
+                                            }, 'date')}
+                                            showYearDropdown
+                                            showMonthDropdown
+                                            minDate={new Date("09-10-1950")}
+                                        />
+                                    </div>
+
+                                    <div className="gender-radio-group">
+                                        {genderOptions.map((option) => (
+                                            <RadioButton
+                                                key={option.value}
+                                                name="gender"
+                                                value={option.value}
+                                                checked={profile.gender === option.value}
+                                                onChange={onChange}
+                                                label={option.label}
+                                            />
+                                        ))}
+                                    </div>
+
+                                    <Button
+                                        type="submit"
+                                        disabled={isSubmitting}
+                                        loading={isSubmitting}
+                                    >
+                                        Update Profile
+                                    </Button>
+
+                                    {user.role === "admin" && (
+                                        <Link to={"/admin"}>
+                                            Admin
+                                        </Link>
+                                    )}
+                                </form>
+                            </>
+                        )}
+
+                </div>
             </div>
-
-            <div className="gender-radio-group">
-              {genderOptions.map((option) => (
-                <RadioButton
-                  key={option.value}
-                  name="gender"
-                  value={option.value}
-                  checked={profile.gender === option.value}
-                  onChange={onChange}
-                  label={option.label}
-                />
-              ))}
-            </div>
-
-            <Button
-              type="submit"
-              disabled={isSubmitting}
-              loading={isSubmitting}
-            >
-              Update Profile
-            </Button>
-
-            {user.role === "admin" && (
-              <Link to={"/admin"}>
-                Admin
-              </Link>
-            )}
-          </form>
-        </>
-      )}
-
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Users;

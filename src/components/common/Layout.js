e9c205fd596a29d59e87c5273f6dedbc0108870d
Narek <narekdarbinyan111@gmail.com>
2025-03-10 19:23:42 +0400

    i modife a search input

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -9,133 +9,141 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMagnifyingGlass, faAngleDown, faCartShopping, faUser} from "@fortawesome/free-solid-svg-icons";
 import {useDispatch, useSelector} from "react-redux";
 import {setIsOpenLogin} from "../../store/actions/login";
-import {setSearchValue} from "../../store/actions/home";
+import {getProducts, setSearchValue} from "../../store/actions/home";
 
 //main
 const token = localStorage.getItem("token");
 
 function Layout() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [user, setUser] = useState({});
-    const [isOpenRegister, setIsOpenRegister] = useState(false)
-    // const [isOpenLogin, setIsOpenLogin] = useState(false)
-    const statusKey = useSelector(state => state.registration.statusKey)
-    const isOpenLogin = useSelector(state => state.login.isOpenLogin)
-    const searchValue = useSelector(state => state.home.searchValue);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+  const [isOpenRegister, setIsOpenRegister] = useState(false)
+  // const [isOpenLogin, setIsOpenLogin] = useState(false)
+  const statusKey = useSelector(state => state.registration.statusKey)
+  const isOpenLogin = useSelector(state => state.login.isOpenLogin)
+  const searchValue = useSelector(state => state.home.searchValue);
+  const selectId = useSelector(state => state.home.selectId);
+  const page = useSelector(state => state.home.page);
+  const minPrice = useSelector(state => state.home.minPrice);
+  const maxPrice = useSelector(state => state.home.maxPrice);
+  const [limit, setLimit] = useState(12);
 
 
-    const onChange = (e) => {
-        if (e.target.value !== " ") {
-            dispatch(setSearchValue(e.target.value));
-        }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (searchValue.trim() !== " ") {
+      navigate("/products");
+      dispatch(getProducts({categoryId: selectId, page, limit, minPrice, maxPrice, s: searchValue || " "}));
     }
-    const search = () => {
-        navigate("/products")
-    }
+  };
 
 
-    return (
-        <>
-            <div className="wrapper">
-                <header className="header">
-                    <div className="container-header">
-                        <Link to="/" className="logo-block">
-                            <div className="logo">
-                                Logo
-                            </div>
-                        </Link>
+  return (
+    <>
+      <div className="wrapper">
+        <header className="header">
+          <div className="container-header">
+            <Link to="/" className="logo-block">
+              <div className="logo">
+                Logo
+              </div>
+            </Link>
 
-                        <nav className="nav">
-                            <ul className="nav-list">
-                                <li className="nav-item">Store
-                                    <FontAwesomeIcon icon={faAngleDown} className="store-arrow"/>
-                                    <ul className="nav-more">
-                                        <li><img src="#"/>IDEAL</li>
-                                        <li><img src="#"/>DOMUS</li>
-                                        <li><img src="#"/>ESIM</li>
-                                    </ul>
-                                </li>
+            <nav className="nav">
+              <ul className="nav-list">
+                <li className="nav-item">Store
+                  <FontAwesomeIcon icon={faAngleDown} className="store-arrow"/>
+                  <ul className="nav-more">
+                    <li><img src="#"/>IDEAL</li>
+                    <li><img src="#"/>DOMUS</li>
+                    <li><img src="#"/>ESIM</li>
+                  </ul>
+                </li>
 
-                                <Link className="nav-item" to="/products">
-                                    <li>Products</li>
-                                </Link>
-                                <Link className="nav-item" to="/category">
-                                    <li>Category</li>
-                                </Link>
-                                <Link className="nav-item" to="/#">
-                                    <li>Specialist</li>
-                                </Link>
-                                <Link className="nav-item" to="/#">
-                                    <li>Contact</li>
-                                </Link>
-                            </ul>
+                <Link className="nav-item" to="/products">
+                  <li>Products</li>
+                </Link>
+                <Link className="nav-item" to="/category">
+                  <li>Category</li>
+                </Link>
+                <Link className="nav-item" to="/#">
+                  <li>Specialist</li>
+                </Link>
+                <Link className="nav-item" to="/#">
+                  <li>Contact</li>
+                </Link>
+              </ul>
 
 
-                        </nav>
+            </nav>
 
-                        <div className="search-block">
-                            <form onSubmit={search} className="form-search">
-                                <div className="search-field">
-                                    <FontAwesomeIcon icon={faMagnifyingGlass} className="glass"/>
-                                    <Input  value={searchValue} onChange={onChange} className="search-input"
-                                           placeholder="Search"/>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div className="user-block">
-                            {!token ?
-                                <>
-                                    <div className="sign-block">
-                                        <Button text="LOGIN" className="active-button"
-                                                onClick={() => dispatch(setIsOpenLogin(true))}></Button>
-                                    </div>
-                                    <div className="sign-block"
-                                    >
-                                        <Button text="REGISTER" className="register-button"
-                                                onClick={() => navigate("/register")}></Button>
-                                    </div>
-
-                                </>
-                                :
-                                <>
-                                    <div className="cart">
-                                        <Link to="/#">
-                                            <FontAwesomeIcon icon={faCartShopping} className="cart-icon"/>
-                                        </Link>
-                                    </div>
-                                    <div className="user">
-                                        <Link to="/" className="user-img">
-                                            <FontAwesomeIcon icon={faUser} className="user-icon"/>
-                                        </Link>
-                                    </div>
-                                </>
-                            }
-                        </div>
-                    </div>
-                </header>
-
-                <main className="main">
-                    <Outlet/>
-                </main>
-
-                <footer className="footer">
-
-                </footer>
+            <div className="search-block">
+              <form onSubmit={handleSearch} className="form-search">
+                <div className="search-field">
+                  <FontAwesomeIcon icon={faMagnifyingGlass} className="glass"/>
+                  <Input
+                    value={searchValue}
+                    onChange={(e) => dispatch(setSearchValue(e.target.value))}
+                    className="search-input"
+                    placeholder="Search"
+                  />
+                </div>
+              </form>
             </div>
 
-            <ModalRegister
-                open={isOpenRegister} onClose={() => {
-                setIsOpenRegister(false)
-            }}/>
-            <ModalLogin
-                open={isOpenLogin} onClose={() => {
-                dispatch(setIsOpenLogin(false))
-            }}/>
-        </>
+            <div className="user-block">
+              {!token ?
+                <>
+                  <div className="sign-block">
+                    <Button text="LOGIN" className="active-button"
+                            onClick={() => dispatch(setIsOpenLogin(true))}></Button>
+                  </div>
+                  <div className="sign-block"
+                  >
+                    <Button text="REGISTER" className="register-button"
+                            onClick={() => navigate("/register")}></Button>
+                  </div>
 
-    );
+                </>
+                :
+                <>
+                  <div className="cart">
+                    <Link to="/#">
+                      <FontAwesomeIcon icon={faCartShopping} className="cart-icon"/>
+                    </Link>
+                  </div>
+                  <div className="user">
+                    <Link to="/" className="user-img">
+                      <FontAwesomeIcon icon={faUser} className="user-icon"/>
+                    </Link>
+                  </div>
+                </>
+              }
+            </div>
+          </div>
+        </header>
+
+        <main className="main">
+          <Outlet/>
+        </main>
+
+        <footer className="footer">
+
+        </footer>
+      </div>
+
+      <ModalRegister
+        open={isOpenRegister} onClose={() => {
+        setIsOpenRegister(false)
+      }}/>
+      <ModalLogin
+        open={isOpenLogin} onClose={() => {
+        dispatch(setIsOpenLogin(false))
+      }}/>
+    </>
+
+  );
 }
 
 export default Layout;
--- src/components/common/Products.js
+++ src/components/common/Products.js
@@ -1,9 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import Product from "../mini/Product";
 import {useDispatch, useSelector} from "react-redux";
-import {getProducts} from "../../store/actions/home";
+import {getProducts, setSelectId, setSearchValue} from "../../store/actions/home";
 import ReactPaginate from "react-paginate";
-import {setSelectId} from "../../store/actions/home";
 import Select from "react-select";
 import {categoriesRequest, setMaxPrice, setMinPrice, setPage} from "../../store/actions/products";
 import {useParams} from "react-router-dom";
@@ -11,7 +10,7 @@ import Slider from "react-slider";
 
 const Products = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [selectValue, setSelectValue] = useState({id: "", name: "All",});
+  const [selectValue, setSelectValue] = useState({id: "", name: "All"});
   const dispatch = useDispatch();
   const products = useSelector(state => state.home.productsList);
   const categories = useSelector(state => state.products.categories);
@@ -25,62 +24,38 @@ const Products = () => {
   const maxPrice = useSelector(state => state.home.maxPrice);
   const searchValue = useSelector(state => state.home.searchValue);
 
-
   useEffect(() => {
     dispatch(categoriesRequest({limit}));
   }, [dispatch, limit]);
 
-
-  const getAllProducts = () => {
-    if (isSubmitted) {
-
-      dispatch(getProducts({categoryId: selectId, page: 1, limit, minPrice, maxPrice, s: searchValue ? searchValue : " "}));
-    } else {
-      dispatch(getProducts({categoryId: selectId, page, limit, minPrice, maxPrice,s: searchValue ? searchValue : " "}));
-    }
-  }
-
   useEffect(() => {
-    getAllProducts()
-  }, [page, limit, searchValue]);
-  console.log(searchValue)
+    dispatch(getProducts({categoryId: selectId, page, limit, minPrice, maxPrice, s: " "}));
+  }, [page, limit,]);
 
   const clearAllOptions = () => {
-    dispatch(setMinPrice(0))
-    dispatch(setMaxPrice(2000))
-    setSelectValue("All")
-    dispatch(setSelectId(""))
-    dispatch(setPage(1))
-  }
-
+    dispatch(setMinPrice(0));
+    dispatch(setMaxPrice(2000));
+    setSelectValue("All");
+    dispatch(setSelectId(""));
+    dispatch(setPage(1));
+    dispatch(setSearchValue(" "));
+  };
 
   const handleClick = (pageInfo) => {
-    let currentPage = pageInfo.selected + 1;
-    dispatch(setPage(currentPage));
+    dispatch(setPage(pageInfo.selected + 1));
   };
 
   const handleSliderChange = (value) => {
-
     dispatch(setMinPrice(value[0]));
     dispatch(setMaxPrice(value[1]));
-
   };
 
-
-  const change = (id) => {
+  const handleCategoryChange = (id) => {
     dispatch(setSelectId(id.id));
-    setSelectValue(name)
-    setIsSubmitted(true)
+    setSelectValue(name);
+    setIsSubmitted(true);
+  };
 
-  };
-  const style = {
-    control: base => ({
-      ...base,
-      border: 0,
-      // This line disable the blue border
-      boxShadow: 'none'
-    })
-  };
   return (
     <div className="wrapper">
       <div className="section">
@@ -88,43 +63,18 @@ const Products = () => {
           <form action="#" className="price-container">
             <span style={{marginTop: "10px", marginBottom: "15px"}}>Price</span>
             <div>
-              <input
-                type="text"
-                className="price-input"
-                value={minPrice}
-                onChange={(e) => handleSliderChange([+e.target.value, maxPrice])}
-              />
-              <input
-                type="text"
-                className="price-input"
-                value={maxPrice}
-                onChange={(e) => handleSliderChange([minPrice, +e.target.value])}
-              />
+              <input type="text" className="price-input" value={minPrice} onChange={(e) => handleSliderChange([+e.target.value, maxPrice])} />
+              <input type="text" className="price-input" value={maxPrice} onChange={(e) => handleSliderChange([minPrice, +e.target.value])} />
             </div>
-
-            <Slider
-              className="price-slider"
-              onChange={handleSliderChange}
-              value={[minPrice, maxPrice]}
-              min={0}
-              max={2000}
-            />
+            <Slider className="price-slider" onChange={handleSliderChange} value={[minPrice, maxPrice]} min={0} max={2000} />
           </form>
-
           <div className="buttons-container">
-            <button onClick={() => getAllProducts()} className="agree-button">
-              Apply
-            </button>
-
-            <button onClick={() => clearAllOptions()} className="agree-button">
-              Clear All
-            </button>
+            <button onClick={() => dispatch(getProducts({categoryId: selectId, page: 1, limit, minPrice, maxPrice, s: searchValue}))} className="agree-button">Apply</button>
+            <button onClick={() => clearAllOptions()} className="agree-button">Clear All</button>
           </div>
-
-
           <div className="select_box">
             <Select
-              onChange={change}
+              onChange={handleCategoryChange}
               placeholder={selectId ? name : "All"}
               options={categories}
               classNamePrefix="react-select"
@@ -138,30 +88,25 @@ const Products = () => {
                   cursor: "pointer",
                   backgroundColor: isFocused ? "limegreen" : "white",
                   color: isFocused ? "white" : "black",
-                  "&:active": {
-                    background: "#84e984",
-                    color:"white",
-                  }
+                  "&:active": { background: "#84e984", color: "white" }
                 }),
-
                 control: (base, { isFocused }) => ({
                   ...base,
                   cursor: "pointer",
                   border: isFocused ? "1px solid limegreen" : "1px solid #cccccc",
                   boxShadow: isFocused ? "0px 0px 6px limegreen" : "none",
-                  '&:hover': {
-                    border: '1px solid limegreen',
-                    boxShadow: '0px 0px 6px limegreen',
-                  },
-                }),
+                  '&:hover': { border: '1px solid limegreen', boxShadow: '0px 0px 6px limegreen' }
+                })
               }}
             />
-
-
           </div>
         </div>
         <div className="products_container">
-          <Product products={products} className="product-block" classNameImg="product-img"/>
+          {products.length > 0 ? (
+            <Product products={products} className="product-block" classNameImg="product-img"/>
+          ) : (
+            <p className="no-products">No products found.</p>
+          )}
           <div className="react_pagination_div">
             <ReactPaginate
               previousLabel={"<"}
@@ -185,3 +130,5 @@ const Products = () => {
 };
 
 export default Products;
+
+

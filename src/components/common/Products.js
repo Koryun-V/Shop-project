02328ec1ab107a88ace9deca0f5a74817bd0d7c2
Koryun-V <koryun.vardanyan.2002@gmail.com>
2025-03-17 18:42:21 +0400

    p-filter

--- src/components/common/Products.js
+++ src/components/common/Products.js
@@ -10,148 +10,161 @@ import Slider from "react-slider";
 import domus from "../../assets/image/domus.png"
 
 const Products = () => {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [selectValue, setSelectValue] = useState({id: "", name: "All"});
-  const dispatch = useDispatch();
-  const products = useSelector(state => state.home.productsList);
-  const categories = useSelector(state => state.products.categories);
-  const selectId = useSelector(state => state.home.selectId);
-  const {name} = useParams();
-  const total = useSelector(state => state.home.total);
-  const [limit, setLimit] = useState(12);
-  const pageCount = Math.ceil(total / limit);
-  const page = useSelector(state => state.home.page);
-  const minPrice = useSelector(state => state.home.minPrice);
-  const maxPrice = useSelector(state => state.home.maxPrice);
-  const searchValue = useSelector(state => state.home.searchValue);
-  const storesList = useSelector(state => state.home.storesList);
-  const storeId = useSelector(state => state.home.storeId);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [selectValue, setSelectValue] = useState({id: "", name: "All"});
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.home.productsList);
+    const categories = useSelector(state => state.products.categories);
+    const selectId = useSelector(state => state.home.selectId);
+    const {name} = useParams();
+    const total = useSelector(state => state.home.total);
+    const [limit, setLimit] = useState(12);
+    const pageCount = Math.ceil(total / limit);
+    const page = useSelector(state => state.home.page);
+    const minPrice = useSelector(state => state.home.minPrice);
+    const maxPrice = useSelector(state => state.home.maxPrice);
+    const searchValue = useSelector(state => state.home.searchValue);
+    const storesList = useSelector(state => state.home.storesList);
+    const storeId = useSelector(state => state.home.storeId);
 
-  useEffect(() => {
-    dispatch(categoriesRequest({limit}));
-  }, [dispatch, limit]);
-
-
-  useEffect(() => {
-    dispatch(getStores({page: 1, limit: 10}))
-
-  }, []);
+    useEffect(() => {
+        dispatch(categoriesRequest({limit}));
+    }, [dispatch, limit]);
 
 
+    useEffect(() => {
+        dispatch(getStores({page: 1, limit: 10}))
 
-  console.log(storeId)
-
-
-
-
-
-  useEffect(() => {
-    dispatch(getProducts({categoryId: selectId, page, limit, minPrice, maxPrice, s: " ", storeId}));
-  }, [page, limit, storeId]);
-
-  const clearAllOptions = () => {
-    dispatch(setMinPrice(0));
-    dispatch(setMaxPrice(2000));
-    setSelectValue("All");
-    dispatch(setSelectId(""));
-    dispatch(setPage(1));
-    dispatch(setSearchValue(" "));
-    dispatch(setStoreId(""))
-  };
+    }, []);
 
 
-  const handleClick = (pageInfo) => {
-    dispatch(setPage(pageInfo.selected + 1));
-  };
+    console.log(storeId)
 
-  const handleSliderChange = (value) => {
-    dispatch(setMinPrice(value[0]));
-    dispatch(setMaxPrice(value[1]));
-  };
 
-  const handleCategoryChange = (id) => {
-    dispatch(setSelectId(id.id));
-    setSelectValue(name);
-    setIsSubmitted(true);
-  };
+    useEffect(() => {
+        dispatch(getProducts({categoryId: selectId, page, limit, minPrice, maxPrice, s: " ", storeId}));
+    }, [page, limit, storeId]);
 
-  return (
-    <div className="wrapper">
-      <div className="section">
-        <div className="select_container">
-          <form action="#" className="price-container">
-            <span style={{marginTop: "10px", marginBottom: "15px"}}>Price</span>
-            <div>
-              <input type="text" className="price-input" value={minPrice} onChange={(e) => handleSliderChange([+e.target.value, maxPrice])} />
-              <input type="text" className="price-input" value={maxPrice} onChange={(e) => handleSliderChange([minPrice, +e.target.value])} />
+    const clearAllOptions = () => {
+        dispatch(setMinPrice(0));
+        dispatch(setMaxPrice(2000));
+        setSelectValue("All");
+        dispatch(setSelectId(""));
+        dispatch(setPage(1));
+        dispatch(setSearchValue(" "));
+        dispatch(setStoreId(""))
+    };
+
+
+    const handleClick = (pageInfo) => {
+        dispatch(setPage(pageInfo.selected + 1));
+    };
+
+    const handleSliderChange = (value) => {
+        dispatch(setMinPrice(value[0]));
+        dispatch(setMaxPrice(value[1]));
+    };
+
+    const handleCategoryChange = (id) => {
+        dispatch(setSelectId(id.id));
+        setSelectValue(name);
+        setIsSubmitted(true);
+    };
+
+    return (
+        <div className="wrapper">
+            <div className="section">
+                <div className="select_container">
+                    <div className="filter">
+
+                            <div className="stores_container">
+                                {storesList.map((item) => (
+
+                                    <div onClick={() => dispatch(setStoreId(item.id))} key={item.id}
+                                         className="stores_item">
+                                        <img className="stores_item_img"
+                                             src={item.storeLogo.length > 0 ? item.storeLogo[0].path : domus}/>
+                                    </div>
+
+                        ))}
+                    </div>
+                    <form action="#" className="price-container">
+                        <span style={{marginTop: "10px", marginBottom: "15px"}}>Price</span>
+                        <div>
+                            <input type="text" className="price-input" value={minPrice}
+                                   onChange={(e) => handleSliderChange([+e.target.value, maxPrice])}/>
+                            <input type="text" className="price-input" value={maxPrice}
+                                   onChange={(e) => handleSliderChange([minPrice, +e.target.value])}/>
+                        </div>
+                        <Slider className="price-slider" onChange={handleSliderChange} value={[minPrice, maxPrice]}
+                                min={0} max={2000}/>
+                    </form>
+
+                    <div className="select_box">
+                        <Select
+                            onChange={handleCategoryChange}
+                            placeholder={selectId ? name : "All"}
+                            options={categories}
+                            classNamePrefix="react-select"
+                            getOptionValue={(o) => o.id}
+                            getOptionLabel={(o) => o.name}
+                            isSearchable={false}
+                            value={selectValue}
+                            styles={{
+                                option: (base, {isFocused}) => ({
+                                    ...base,
+                                    cursor: "pointer",
+                                    backgroundColor: isFocused ? "limegreen" : "white",
+                                    color: isFocused ? "white" : "black",
+                                    "&:active": {background: "#84e984", color: "white"}
+                                }),
+                                control: (base, {isFocused}) => ({
+                                    ...base,
+                                    cursor: "pointer",
+                                    border: isFocused ? "1px solid limegreen" : "1px solid #cccccc",
+                                    boxShadow: isFocused ? "0px 0px 6px limegreen" : "none",
+                                    '&:hover': {border: '1px solid limegreen', boxShadow: '0px 0px 6px limegreen'}
+                                })
+                            }}
+                        />
+                    </div>
+                    <div className="buttons-container">
+                        <button onClick={() => dispatch(getProducts({
+                            categoryId: selectId,
+                            page: 1,
+                            limit,
+                            minPrice,
+                            maxPrice,
+                            s: searchValue
+                        }))} className="agree-button">Apply
+                        </button>
+                        <button onClick={() => clearAllOptions()} className="agree-button">Clear All</button>
+                    </div>
+                </div>
+
             </div>
-            <Slider className="price-slider" onChange={handleSliderChange} value={[minPrice, maxPrice]} min={0} max={2000} />
-          </form>
-
-          <div className="select_box">
-            <Select
-              onChange={handleCategoryChange}
-              placeholder={selectId ? name : "All"}
-              options={categories}
-              classNamePrefix="react-select"
-              getOptionValue={(o) => o.id}
-              getOptionLabel={(o) => o.name}
-              isSearchable={false}
-              value={selectValue}
-              styles={{
-                option: (base, { isFocused }) => ({
-                  ...base,
-                  cursor: "pointer",
-                  backgroundColor: isFocused ? "limegreen" : "white",
-                  color: isFocused ? "white" : "black",
-                  "&:active": { background: "#84e984", color: "white" }
-                }),
-                control: (base, { isFocused }) => ({
-                  ...base,
-                  cursor: "pointer",
-                  border: isFocused ? "1px solid limegreen" : "1px solid #cccccc",
-                  boxShadow: isFocused ? "0px 0px 6px limegreen" : "none",
-                  '&:hover': { border: '1px solid limegreen', boxShadow: '0px 0px 6px limegreen' }
-                })
-              }}
-            />
-          </div>
-          <div className="buttons-container">
-            <button onClick={() => dispatch(getProducts({categoryId: selectId, page: 1, limit, minPrice, maxPrice, s: searchValue}))} className="agree-button">Apply</button>
-            <button onClick={() => clearAllOptions()} className="agree-button">Clear All</button>
-          </div>
-
-          <div className="stores_container">
-            {storesList.map((item) => (
-              <div onClick={() => dispatch(setStoreId(item.id))} key={item.id} className="stores_item">
-                <img className="stores_item_img" src= {item.storeLogo.length > 0 ? item.storeLogo[0].path : domus } alt="store_img"/>
-              </div>
-            ))}
-          </div>
-
+            <div className="products_container">
+                <Product products={products} className="product-block" classNameImg="product-img"/>
+                <div className="react_pagination_div">
+                    <ReactPaginate
+                        previousLabel={"<"}
+                        nextLabel={pageCount ? ">" : ""}
+                        pageCount={pageCount}
+                        pageRangeDisplayed={3}
+                        onPageChange={handleClick}
+                        pageLinkClassName={"page-link"}
+                        containerClassName={"pagination"}
+                        pageClassName={"page-item"}
+                        activeClassName={"page-item--active"}
+                        previousClassName={"page-item--previous"}
+                        nextClassName={"page-item--next"}
+                        forcePage={page - 1}
+                    />
+                </div>
+            </div>
         </div>
-        <div className="products_container">
-            <Product products={products} className="product-block" classNameImg="product-img"/>
-          <div className="react_pagination_div">
-            <ReactPaginate
-              previousLabel={"<"}
-              nextLabel={pageCount ? ">" : ""}
-              pageCount={pageCount}
-              pageRangeDisplayed={3}
-              onPageChange={handleClick}
-              pageLinkClassName={"page-link"}
-              containerClassName={"pagination"}
-              pageClassName={"page-item"}
-              activeClassName={"page-item--active"}
-              previousClassName={"page-item--previous"}
-              nextClassName={"page-item--next"}
-              forcePage={page - 1}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+</div>
+);
 };
 
 export default Products;
